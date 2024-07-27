63aeefe23307c3985a804c5b66618570ffff6ac5
byNico1 <nicofoque0205@gmail.com>
2024-07-27 12:22:49 -0500

    add flipping to cards when clicked

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,24 @@
+// import Tilt from "react-parallax-tilt";
 import "./Card.css";
 
-const Card = ({ pokemon, handleCardClick }) => {
+const Card = ({ pokemon, handleCardClick, shuffling }) => {
   return (
-    <div className="card" onClick={() => handleCardClick(pokemon)}>
-      <h2>{pokemon.name}</h2>
-      <img
-        width={200}
-        height={200}
-        src={pokemon.sprites.front_default}
-        alt={pokemon.name}
-      />
+    <div
+      className={shuffling ? `card card-shuffling` : `card`}
+      onClick={() => handleCardClick(pokemon)}
+    >
+      <div className="tilt">
+        <div className="card-front">
+          <h2>{pokemon.name}</h2>
+          <img
+            width={200}
+            height={200}
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
+        </div>
+        <div className="card-back"></div>
+      </div>
     </div>
   );
 };
